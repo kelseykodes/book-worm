88feef5852e8b9aa1387200ce53a2bfe4c2f7887
kperkins97 <perkins.kelsey@yahoo.com>
2022-02-05 15:27:21 -0600

    deploying whole new app

---
+++ server/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path = require('path');
+const db = require('./config/connection');
+// const routes = require('./routes');
+//Updating the auth middleware function to work with the GraphQL API//
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+        context: authMiddleware
+    });
+    server.applyMiddleware({ app });
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`gql path is ${apolloServer.graphqlPath}`);
+  });
+});
